3147590da138636833c855f9b3b898f1d7a6ec52
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-17 14:55:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Upload from "./Upload";
+// import About from './About';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Upload />
+      {/* <About/> */}
+    </div>
+  );
+}
+export default App;
