af55288a6ff2f34b482ee89e6ee7691176ddb582
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-28 15:21:04 +0530

    Initial commit

--- src/Upload.js
+++ src/Upload.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FileDrop } from "react-file-drop";
+import { drawRect } from "./utilities";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 import "./Upload.css";
 
 function Upload(props) {
@@ -8,6 +11,26 @@ function Upload(props) {
     src: null,
     alt: "Upload an Image",
   });
+  const canvasRef = useRef(null);
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("Handpose model loaded.");
+    detect(net);
+  };
+  const detect = async (net) => {
+    const img = document.getElementById("testimage");
+    const width = 600;
+    const height = 300;
+    canvasRef.current.width = width;
+    canvasRef.current.height = height;
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.drawImage(img, 0, 0, width, height);
+    const obj = await net.detect(img);
+    console.log("ctx => ", ctx);
+    console.log("obj => ", obj);
+    drawRect(obj, ctx);
+  };
 
   const onFileChange = (e) => {
     console.log(e.target.files[0]);
@@ -33,7 +56,7 @@ function Upload(props) {
       return (
         <div>
           <div className="box-preview">
-            <img src={src} alt={alt}></img>
+            <img id="testimage" src={src} alt={alt}></img>
           </div>
           <h4 className="uploadTxt">File Uploaded.</h4>
         </div>
@@ -69,7 +92,16 @@ function Upload(props) {
           id="upload-btn"
           onClick={onFileUpload}
         />
-        <label className="btn-fnc" htmlFor="upload-btn">
+        <input
+          type="button"
+          style={{ display: "none" }}
+          id="analyze-btn"
+          onClick={() => {
+            console.log("start");
+            runCoco();
+          }}
+        />
+        <label className="btn-fnc" htmlFor="analyze-btn">
           Analyze
         </label>
       </div>
@@ -88,6 +120,20 @@ function Upload(props) {
       >
         {fileData()}
       </FileDrop>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 8,
+          width: 600,
+          height: 300,
+        }}
+      />
     </div>
   );
 }
